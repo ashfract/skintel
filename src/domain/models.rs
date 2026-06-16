017a41755fa52b2d0f53399f4abe607f65b3b9c4
ashfract <callum.bit@proton.me>
2026-06-16 18:24:32 +0100

    feat: implemented from_str method for Rarity to get Enum variant from a string

--- src/domain/models.rs
+++ src/domain/models.rs
@@ -1,5 +1,7 @@
 use serde::Deserialize;
 
+use crate::domain::models::Rarity::ExceedinglyRare;
+
 #[derive(Deserialize, Debug)]
 pub enum Rarity {
     ConsumerGrade,
@@ -9,6 +11,21 @@ pub enum Rarity {
     Classified,
     Covert,
     ExceedinglyRare,
+}
+
+impl Rarity {
+    fn from_str(s: &str) -> Option<Rarity> {
+        match s {
+            "Consumer Grade" => Some(Rarity::ConsumerGrade),
+            "Industrial Grade" => Some(Rarity::IndustrialGrade),
+            "Mil Spec" => Some(Rarity::MilSpec),
+            "Restricted" => Some(Rarity::Restricted),
+            "Classified" => Some(Rarity::Classified),
+            "Covert" => Some(Rarity::Covert),
+            "Exceedingly Rare" => Some(Rarity::ExceedinglyRare),
+            _ => None,
+        }
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -22,10 +39,13 @@ pub enum Condition {
 
 #[derive(Deserialize, Debug)]
 pub struct Skin {
-    pub name: String,
-    pub collection: String,
+    pub hash_name: String,
+    pub collection: Option<String>,
     pub rarity: Rarity,
     pub stattrak: bool,
     pub min_float: f32,
     pub max_float: f32,
+    pub sell_price: u32,
+    pub formatted_price: String,
+    pub sell_listings: u32,
 }
