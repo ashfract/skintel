283b085235a4fdaac55589e706d86738eabd6725
ashfract <callum.bit@proton.me>
2026-06-17 15:35:49 +0100

    feat: rarity from string function and api return changed to Skin

--- src/data/bymekel.rs
+++ src/data/bymekel.rs
@@ -1,5 +1,8 @@
+use crate::models;
 use reqwest;
 use serde::Deserialize;
+
+use crate::models::Rarity;
 
 #[derive(Deserialize, Debug)]
 pub struct RarityWrapper {
@@ -9,7 +12,7 @@ pub struct RarityWrapper {
 #[derive(Deserialize, Debug)]
 pub struct Metadata {
     #[serde(rename = "name")]
-    pub hash_name: String,
+    pub market_hash_name: String,
     pub min_float: Option<f32>,
     pub max_float: Option<f32>,
     #[serde(rename = "rarity")]
@@ -17,9 +20,23 @@ pub struct Metadata {
     pub stattrak: bool,
 }
 
-pub async fn fetch_metadata() -> Result<Vec<Metadata>, Box<dyn std::error::Error>> {
+pub async fn fetch_metadata() -> Result<Vec<models::Skin>, Box<dyn std::error::Error>> {
     let url = "https://raw.githubusercontent.com/ByMykel/CSGO-API/main/public/api/en/skins_not_grouped.json";
     let data: Vec<Metadata> = reqwest::get(url).await?.json::<Vec<Metadata>>().await?;
+
+    let skins = data
+        .into_iter()
+        .filter_map(|m| {
+            Some(models::Skin {
+                market_hash_name: m.market_hash_name,
+                collection: String::new(),
+                min_float: m.min_float? as f64,
+                max_float: m.max_float? as f64,
+                rarity: Rarity::from_str(&m.rarity.name)?,
+                stattrak: m.stattrak,
+            })
+        })
+        .collect();
     println!("Bymykel API fetched successfully");
-    Ok(data)
+    Ok(skins)
 }
--- src/models.rs
+++ src/models.rs
@@ -1,1 +1,33 @@
 use serde::Deserialize;
+
+pub enum Rarity {
+    ConsumerGrade,
+    IndustrialGrade,
+    MilSpec,
+    Restricted,
+    Classified,
+    Covert,
+}
+
+impl Rarity {
+    pub fn from_str(s: &str) -> Option<Rarity> {
+        match s {
+            "Consumer Grade" => Some(Rarity::ConsumerGrade),
+            "Industrial Grade" => Some(Rarity::IndustrialGrade),
+            "MilSpec Grade" => Some(Rarity::MilSpec),
+            "Restricted" => Some(Rarity::Restricted),
+            "Classified" => Some(Rarity::Classified),
+            "Covert" => Some(Rarity::Covert),
+            _ => None,
+        }
+    }
+}
+
+pub struct Skin {
+    pub market_hash_name: String,
+    pub collection: String,
+    pub min_float: f64,
+    pub max_float: f64,
+    pub rarity: Rarity,
+    pub stattrak: bool,
+}
